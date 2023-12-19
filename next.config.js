70c334d4e36866ae1041280196d0db9c091a6758
Dam Anh Tuan <anhtuantb2422@gmail.com>
2023-12-19 11:43:44 +0700

    Done

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'pbxt.replicate.delivery',
+      'kiqwqdd7sey6lref.public.blob.vercel-storage.com',
+    ],
+  },
+};
+
+module.exports = nextConfig;
